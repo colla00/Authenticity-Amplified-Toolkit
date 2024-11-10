973bcda34c1b8cb23329d4b5e945f525b8f3c609
Portfolio of Projects by Alexis <info@alexiscollier.com>
2024-11-09 21:40:15 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Authenticity Amplified: The Storyteller's Toolkit</h1>
+            <p>Welcome to the storyteller's toolkit, a place for authentic and creative expression.</p>
+        </div>
+    );
+}
+
+export default App;
